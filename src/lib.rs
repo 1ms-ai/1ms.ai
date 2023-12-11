781ee0d9d8173a96c14f868e5d2530dba022b932
haixuanTao <tao.xavier@outlook.com>
2023-12-11 15:48:32 +0100

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+use wasm_bindgen::prelude::*;
+
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("h2")?;
+    val.set_text_content(Some("1ms.ai is getting started!"));
+    body.append_child(&val)?;
+
+    // let val = document.create_element("a")?;
+    // val.set_text_content(Some("> jobs?"));
+    // val.set_attribute("href", "www.google.com")?;
+    // body.append_child(&val)?;
+
+    // let val = document.create_element("p")?;
+    // val.set_text_content(Some("> Blogs?"));
+    // body.append_child(&val)?;
+    Ok(())
+}
